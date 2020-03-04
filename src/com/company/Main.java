2671de0ab6fc8065c42130bf558cf105893f78e3
Krzysiek <celiko_kg@live.com>
2020-03-04 18:41:24 +0100

    suma wszystkich elementow tablicy, podstawowe pętle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,15 +5,35 @@ public class Main {
     public static void main(String[] args) {
 
         int[] tab = new int[50];
-        int licznik = 0;
-
+        int suma = 0;
+        //Pętla for
         for (int i = 0; i < 50; i++) {
             tab[i] = i;
         }
+        //pętla for typu foreach
         for (int z : tab) {
-            licznik+=z;
+            suma += z;
         }
-        System.out.println(licznik);
+        System.out.println(suma);
+
+        //petla while
+        int licznik = 0;
+        suma = 0;
+        while (licznik != tab.length) {
+            suma += tab[licznik];
+            licznik++;
+        }
+        System.out.println(suma);
+
+        //petla do while
+        licznik = 0;
+        suma = 0;
+        do {
+            suma += tab[licznik];
+            licznik++;
+        }
+        while (licznik != tab.length);
+        System.out.println(suma);
     }
 }
 
